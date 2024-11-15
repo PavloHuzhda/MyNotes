80948d8105091f0b333d5d28424b8f280ea7fb4f
FoOtBaLL3R <pavlo.23.94@gmail.com>
2024-11-15 05:55:31 -0700

    added registration for users and changed the tab icon.

--- MyNotes.Server/Repositories/IUserRepository.cs
+++ MyNotes.Server/Repositories/IUserRepository.cs
@@ -5,7 +5,9 @@ namespace MyNotes.Server.Repositories
     public interface IUserRepository
     {
         Task CreateUserAsync(User user);
-        Task<User> GetUserByIdAsync(string id);
-        Task<User> GetUserByUsernameAsync(string username);
+        Task<User?> GetUserByEmailAsync(string email);
+        Task<User?> GetUserByIdAsync(string id);
+        Task UpdateUserAsync(User user);
+        Task DeleteUserAsync(string id);
     }
 }
